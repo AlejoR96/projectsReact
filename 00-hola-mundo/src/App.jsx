800bcdb821ab9c24af247f468198cd36bf8a2744
Alejandro Rivera <jalejandro142@gmail.com>
2024-03-10 22:47:23 -0500

    Se ajusta hoja de estilos ultilizando className

--- 00-hola-mundo/src/App.jsx
+++ 00-hola-mundo/src/App.jsx
@@ -1,20 +1,25 @@
-import './App.css';
-export function App () {
-  return(
-    <article>
-      <header>
-        <img alt="avatarSG" src="https://unavatar.io/soysantoguerrero"/>
-        <div>
+import "./App.css";
+export function App() {
+  return (
+    <article className="x-followCard">
+      <header className="x-followCard-header">
+        <img
+          className="x-followCard-avatar"
+          alt="avatarSG"
+          src="https://unavatar.io/soysantoguerrero"
+        />
+        <div className="x-followCard-info">
           <strong>Alejandro Rivera</strong>
-          <span>@soysantoguerrero</span>
+          <span className="x-followCard-infoUserName">@soysantoguerrero
+          </span>
         </div>
       </header>
 
       <aside>
-        <button>
+        <button className="x-buttonFollow">
           Seguir
-        </button>
+          </button>
       </aside>
     </article>
-  )
+  );
 }
