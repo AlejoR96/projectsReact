536bdfe4eeffeaf2d91a8297380070633930b525
Alejandro Rivera <jalejandro142@gmail.com>
2024-04-21 01:54:42 -0500

    Se realizo un array de usuarios

--- 00-hola-mundo/src/App.jsx
+++ 00-hola-mundo/src/App.jsx
@@ -1,18 +1,39 @@
 import "./App.css";
 import { XFollowCard } from "./XFollowcard";
 
+const users = [
+  {
+    userName: 'soysantoguerrero',
+    name: 'Alejandro Rivera',
+    isFollowing: true
+  },
+  {
+    userName: 'dvalencia220',
+    name: 'Daniela Valencia',
+    isFollowing: false
+  },
+  {
+    userName: 'julieethaa_',
+    name: 'Julieth ',
+    isFollowing: true
+  },
+]
+
+
 export function App() {
-
   return (
-    <section className="appComponent">
-      <XFollowCard isFollowing userName="soysantoguerrero">
-         Alejandro Rivera 
-      </XFollowCard>
-
-      <XFollowCard isFollowing userName="laurajperezz">
-        Laura Perez
-      </XFollowCard>
-        
+    <section className="appComponent">{
+       
+         users.map(({ userName, name, isFollowing }) => (
+          <XFollowCard 
+           userName={userName}
+           key={userName}
+           initialIsFollowing={isFollowing}
+          >
+          {name}
+          </XFollowCard>
+      ))
+      }
     </section>
   );
 }
